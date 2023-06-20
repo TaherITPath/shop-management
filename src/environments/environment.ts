bc5c9abbf53c3c1e6e3863101a8cf6709c222914
TaherITPath <tahert.itpath@gmail.com>
2023-06-20 11:36:51 +0530

    c

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
-//export const environment = {};
-// export const environment = {
-// production: true,
-// baseApiUrl: 'https://localhost:7216'
-// };
 export const environment = {
-    production: true,
-    baseApiUrl: 'http://localhost:82'
+production: true,
+baseApiUrl: 'https://localhost:7216'
 };
+// export const environment = {
+//     production: true,
+//     baseApiUrl: 'http://localhost:82'
+// };
